4810b630d2dcb0159da1a89754046c4339a49257
ferhathajdarpasic <ferhat.hajdarpasic@live.com.au>
2018-04-18 01:23:56 +1000

    Running Android debug version of demo.

--- App.js
+++ App.js
@@ -1,42 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
-import React, { Component } from 'react';
-import { StyleSheet, NavigatorIOS, Platform } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+import {
+  StackNavigator,
+} from 'react-navigation';
 import SearchPage from './SearchPage';
+import SearchResults from './SearchResults';
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <NavigatorIOS
-        style={styles.container}
-        initialRoute={{
-          title: 'Property Finder',
-          component: SearchPage,
-        }}/>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
-  },
-  container: {
-    flex: 1,
-  },
+const App = StackNavigator({
+  Home: { screen: SearchPage },
+  Results: { screen: SearchResults },
 });
+export default App;
