2b2e4fd6e1653d9032b8543285dcc4fa64f9994d
fhajdarp <ferhat.hajdarpasic@aon.com>
2018-04-23 15:59:28 +1000

    Example of how to use any npm package.

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import { View, TextInput, Image, ImageBackground } from 'react-native';
 
 import { StyleSheet, WebView, Platform} from 'react-native';
 import BKD  from './BobBackground'
+import accounting from 'accounting';
  
 export default class MainActivity extends Component {
- 
       render() {
+        console.log('FRED=' + accounting.formatMoney(455678.678));
         let redirect_url = 'about:blank';
         let client_id='6a878d3c8b854a1387d2bcbe4c665cea';        
         return (     
