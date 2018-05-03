72c2aedc2f449019bca2ed7538c9b6c9199c6b20
ferhathajdarpasic <ferhat.hajdarpasic@live.com.au>
2018-05-04 00:44:23 +1000

    import from spotify now uses flatlist.

--- ImportFromSpotify.js
+++ ImportFromSpotify.js
@@ -5,6 +5,7 @@ import { View, Text, TextInput, Image, ImageBackground } from 'react-native';
 import { StyleSheet, WebView, Platform } from 'react-native';
 import BKD from './BobBackground'
 import SpotifyApi from './SpotifyApi'
+import FlatListDemo from './FlatListDemo'
 
 let api = new SpotifyApi();
 
@@ -22,16 +23,15 @@ export default class ImportFromSpotify extends Component {
           flex: 1,
           flexDirection: 'column',
         }}>
-          <View style={{ flexDirection: 'row', backgroundColor: 'transparent', flex: 2 }}>
+          <View style={{ flexDirection: 'row', backgroundColor: 'transparent', flex: 1.5 }}>
           </View>
-          <View style={{ flexDirection: 'row', backgroundColor: 'powderblue', flex: 1, alignItems: 'center' }}>
+          <View style={{ flexDirection: 'row', backgroundColor: 'transparent', flex: 1, alignItems: 'center' }}>
             <Image source={require('./Resources/3RD_PARTY_LOGOS/SPOTIFY.png')} style={styles.spotify} />
           </View>
-          <View style={{ flexDirection: 'row', backgroundColor: 'skyblue', flex: 6, alignItems: 'center' }}>
-            <Image source={require('./Resources/3RD_PARTY_LOGOS/GOOGLE.png')} style={styles.spotify} />
-            <Image source={require('./Resources/3RD_PARTY_LOGOS/FB.png')} style={styles.facebook} />
+          <View style={{ flexDirection: 'row', backgroundColor: 'transparent', flex: 6, marginLeft:'10%' }}>
+            <FlatListDemo />
           </View>
-          <View style={{ flexDirection: 'row', backgroundColor: 'steelblue', flex: 1, alignItems: 'center' }}>
+          <View style={{ flexDirection: 'row', backgroundColor: 'transparent', flex: 1, marginLeft:'10%', alignItems: 'center' }}>
             <Image source={require('./Resources/BOB_LOGOS/BOB_LOGO_ORANGE.png')} style={styles.titleImage} />
             <Text style={styles.titleText}>import all to bob</Text>
           </View>
@@ -54,15 +54,16 @@ const styles = StyleSheet.create({
   loginToBob: { color: 'white', fontFamily: 'Bauhaus 93', fontSize: 20, marginBottom: '10%' },
   login: { color: '#FCB415', fontFamily: 'Bauhaus 93', fontSize: 20, marginBottom: '10%' },
   import: { color: '#FCB415', fontFamily: 'Bauhaus 93', fontSize: 20 },
-  spotify: { width: 40, height: 40 * (1065 / 1045) },
+  spotify: { width: 40, height: 40 * (1065 / 1045), marginLeft:'17%' },
   facebook: { width: 40, height: 40 },
   titleImage: {
     width: 50,
-    height: (214 / 241) * 50,
+    height: (214 / 241) * 50
   },
   titleText: {
     color: '#FCB415',
     fontFamily: 'Bauhaus 93',
     fontSize: 20,
+    padding:5
   }
 });
