d2b1d09d0d3b8b5d059be165875d5ed7d70c42f6
ferhathajdarpasic <ferhat.hajdarpasic@live.com.au>
2018-06-15 00:16:10 +1000

    Downloaded youtube sound inside of Bob.

--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -2,11 +2,23 @@
 // The main.js file will be overwritten in updates/reinstalls.
 
 var rn_bridge = require('rn-bridge');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const os = require('os');
 
 // Echo every message received from react-native.
 rn_bridge.channel.on('message', (msg) => {
-  rn_bridge.channel.send(msg);
-} );
+  try {
+    let success = fs.existsSync('/storage/emulated/0/Android/data/com.bob/cache');
+    
+    const stream = fs.createWriteStream('/storage/emulated/0/Android/data/com.bob/cache/halloween.flac');
+    ytdl('https://www.youtube.com/watch?v=ek1ePFp-nBI', { filter: 'audioonly' }).pipe(stream);
+    console.log('Freddy');
+    rn_bridge.channel.send(msg + ':' +success + ':' + JSON.stringify(os.homedir())) ;//+ stream.bytesWritten);
+  } catch (e) {
+    rn_bridge.channel.send(e.message);
+  }
+});
 
 // Inform react-native node is initialized.
 rn_bridge.channel.send("Node was initialized.");
